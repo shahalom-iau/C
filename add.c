2d0d93ba8d53398d085abf3bc75045c789e54cc0
SHA <shahalom.iau@gmail.com>
2022-10-24 01:09:36 +0600

    New File Added

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter a Number \n");
+    scanf("%d",&a);
+
+    printf("Enter another Number \n");
+    scanf("%d",&b);
+
+    printf("The Sum of the Number is %d", a+b);
+    return 0;
+}
+
