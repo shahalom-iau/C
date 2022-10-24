42a42d6f16c9030995d0ddde422fc85be6434d8c
SHA <shahalom.iau@gmail.com>
2022-10-24 15:10:36 +0600

    "New Code"

---
+++ format_specifier.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main (){
+
+/* 
+Format Specifier-Type
+
+%c---Used to print the character
+
+%d---Used to print the signed integer
+
+%f---Used to print the float values
+
+%i---Used to print the unsigned integer
+
+%l---Used to long integer
+
+%lf---Used to print the double integer
+
+%lu---Used to print the unsigned int or unsigned long integer
+
+%s---Used to print the String
+
+%u---Used to print the unsigned integer
+*/
+
+
+/*
+Escape Sequence---Description
+
+\t---Inserts a tab
+
+\b---Inserts a backspace
+
+\n---Inserts a newline.
+
+\r---Inserts a carriage return.
+
+\f---Inserts a form feed.
+
+\’---Inserts a single quote character.
+
+\”---Inserts a double quote character.
+
+\\---Inserts a backslash character
+*/
+}
