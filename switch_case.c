c49a2b739e738a4a3ea62b3d5b13a53bd48fadb4
SHA <49731746+shahalom-iau@users.noreply.github.com>
2022-10-25 17:05:56 +0600

    new

---
+++ switch_case.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main(){
+    
+    int a;
+    printf("Please input your age: ");
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf(Your age is %d and you are a baby);
+        break;
+    case 1:
+        printf(Your age is %d and you are a baby);
+        break;
+    case 1:
+        printf(Your age is %d and you are a baby);
+        break;
+    case 1:
+        printf(Your age is %d and you are a baby);
+        break;
+    case 1:
+        printf(Your age is %d and you are a baby);
+        break;
+    case 1:
+        printf(Your age is %d and you are a baby);
+        break;
+    
+    default:
+        break;
+    }
+    
+}
