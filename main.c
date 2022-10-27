67722adaaf7bd34010c55e20d6e074400e349f23
SHA <49731746+shahalom-iau@users.noreply.github.com>
2022-10-27 22:25:52 +0600

    New file

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
  
 int main(){
-    int a,b;
-
-    printf("Please input a number: ");
-    scanf("%d", &a);
-
-    printf("Please input another number: ");
-    scanf("%d", &b); 
-
-    printf("The Sum of the number is: %d",a+b);
-    return 0;
+    printf("Hello World");
 }
