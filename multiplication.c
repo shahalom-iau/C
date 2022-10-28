8c26af707bfa4ec11fbaf62b1405d11b4d4b667b
SHA <49731746+shahalom-iau@users.noreply.github.com>
2022-10-28 14:25:05 +0600

    New

--- multiplication.c
+++ multiplication.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
-    int a, i;
+    int a, i,m;
+    printf("Enter the Number: ");
     scanf("%d", &a);
 
     for(i=1;i<=10;i++){
-        int m=a*i;
+        m=a*i;
         printf("%d * %d = %d\n", a, i, m);
     }
 
