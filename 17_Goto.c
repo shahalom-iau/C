44cdd5ddb9069342a473d8e1259f0319ec0a5718
SHA <shahalom.syl71@gmail.com>
2022-10-29 15:49:24 +0600

    Format Change

---
+++ 17_Goto.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+// GoTo is Mainly used for exiting from every nested loop in a line.
+    label:
+        printf("This is inside label\n");
+        goto end;
+        printf("Hello World");
+        goto label; // This code wont take effect if we give another statement under this.
+    end:
+        printf("This is end\n");
+
+    int num;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+        for (int j = 0; j < 1; j++)
+        {
+            printf("Enter a Number: "); // 0 is for exit
+            scanf("%d", &num);
+            if (num == 0)
+            {
+                goto endloop;
+            }
+        }
+    }
+endloop:
+
+    return 0;
+}
---
+++ 18_Typecasting.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+  
+ int main(){
+    
+    int a =3;
+    printf("The Value of a is %.2f", (float) a);// TypeCasting Syntax :  (Type) integer Value.
+    
+ return 0;
+ }
